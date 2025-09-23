15bee149288ba677c28a6596b3a0b3c3fa2ba242
Shane Caldwell <shanejcaldwell@gmail.com>
2025-09-22 23:21:09 -0500

    Outer step

--- nanodiloco/diloco/diloco.py
+++ nanodiloco/diloco/diloco.py
@@ -14,7 +14,7 @@ class Diloco:
             dist.broadcast(rank_0_param, src=0)
         self._sync_time = 0
         self._sync_calls = 0
-        self.offloaded_parameters = self._get_offloaded_parameters()
+        self.offloaded_last_sync_parameters = self._get_offloaded_parameters()
     
     def _get_offloaded_parameters(self):
         return [
@@ -23,4 +23,43 @@ class Diloco:
             for param in group["params"]
         ]
     
+    def outer_step(self) -> None:
+        """
+        Outer step for Diloco.
+        Loads last sync parameters from CPU to GPU and computes the psuedo-gradient for outer optimizer.
+        Updates the offloaded parameters to CPU.
+        """
+        replica_params = [
+            param
+            for group in self.inner_optimizer.param_groups
+            for param in group["params"]
+        ]
+
+        for replica_param, last_sync_param in zip(replica_params, self.offloaded_last_sync_parameters):
+            last_sync_param_on_device = last_sync_param.to(replica_param.device)
+            last_sync_param.grad = last_sync_param_on_device - replica_param.data
+            dist.all_reduce(tensor=last_sync_param.grad, op=dist.ReduceOp.AVG)
+            replica_param.data = last_sync_param_on_device
+        
+        self.outer_optimizer.step()
+        self.outer_optimizer.zero_grad()
+        self.offloaded_last_sync_parameters = self._get_offloaded_parameters()
+
+    @property
+    def avg_sync_time(self):
+        return self._sync_time / self._sync_calls if self._sync_calls > 0 else 0
     
+    def __call__(self, *args, **kwargs):
+        return self.model(*args, **kwargs)
+    
+    def disable_grad_sync(self):
+        self.sync_grads = False
+
+    def enable_grad_sync(self):
+        self.sync_grads = True
+
+    def train(self):
+        self.model.train()
+
+    def eval(self):
+        self.model.eval()
